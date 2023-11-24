24e08c57dfeca8579669b70c7cd9b23d8eb30885
ryandobkin <91717024+ryandobkin@users.noreply.github.com>
2023-11-24 13:27:55 -0800

    Add files via upload

---
+++ portfolio/website_js.js
@@ -0,0 +1,10 @@
+var id = setInterval(frame, 5);
+var elem = document.getElementById("title_animation");
+
+function fade_in() {
+    if (elem.opacity == 100){
+        clearInterval(id);
+    } else {
+        elem.opacity += 10;
+    }
+}
